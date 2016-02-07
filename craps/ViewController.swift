df225f3e3b0b37a2c8ff01b9140741f4ddf9747b
miguel a hernandez <voltaire33010@gmail.com>
2016-02-07 11:21:05 -0500

    Initial Commit

---
+++ craps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  craps
+//
+//  Created by miguel hernandez on 2/7/16.
+//  Copyright © 2016 com.securecodetips. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
