cbd928eef767d97cc8bb06087b2c7e13c904cd10
miguel a hernandez <voltaire33010@gmail.com>
2016-02-26 16:59:25 -0500

    segue

--- craps/ViewController.swift
+++ craps/ViewController.swift
@@ -15,15 +15,11 @@ class ViewController: UIViewController {
     
     
     @IBOutlet var diceImage2: UIImageView!
-    //var audioPlayer = AVAudioPlayer()
+   
      var audioPlayer:AVAudioPlayer!
     @IBAction func roll(sender: AnyObject) {
         
-       // let soundURL = NSBundle.mainBundle().URLForResource("DICE.mp3", withExtension: "mp3")
-       // audioPlayer = AVAudioPlayer(contentsOfURL?: soundURL, error: nil)
-       // audioPlayer.play()
-        //var mySound = NSSound(named:"DICE.mp3")
-       // mySound.play()
+       
         let audioFilePath = NSBundle.mainBundle().pathForResource("DICE", ofType: "mp3")
         if audioFilePath != nil {
             let audioFileUrl = NSURL.fileURLWithPath(audioFilePath!)
@@ -35,16 +31,22 @@ class ViewController: UIViewController {
                 audioPlayer.play()
             }
         
+        
+        let screenSize: CGRect = UIScreen.mainScreen().bounds
+        let screenWidth = UInt32(screenSize.width * 0.75)
+        let screenHeight = UInt32(screenSize.height * 0.75)
+        
+        
       let dic1 =  arc4random_uniform(UInt32(6))+1
       let dic2 = arc4random_uniform(UInt32(6))+1
         
-        let poc1 = Int(arc4random_uniform(UInt32(600))+5)
-        let poc2 = Int(arc4random_uniform(UInt32(600))+5)
+       // let poc1 = Int(arc4random_uniform(UInt32(600))+5)
+       //let poc2 = Int(arc4random_uniform(UInt32(600))+5)
+        let poc1 = Int(arc4random_uniform(screenWidth)+5)
+      let poc2 = Int(arc4random_uniform(screenHeight)+5)
         
-        let poc3 = Int(arc4random_uniform(UInt32(600))+5)
-        let poc4 = Int(arc4random_uniform(UInt32(600))+5)
-        //diceImage1.image=UIImage(named:"\(dic1).png")
-        //diceImage2.image=UIImage(named:"\(dic2).png")
+        let poc3 = Int(arc4random_uniform(screenWidth)+5)
+        let poc4 = Int(arc4random_uniform(screenHeight)+5)
         
       
         let image1 : UIImage = UIImage(named:"\(dic1).png")!
@@ -53,7 +55,6 @@ class ViewController: UIViewController {
        
         diceImage1.frame = CGRect(x: poc1, y: poc2, width: 50, height: 50)
         view.addSubview(diceImage1)
-        //diceImage1.image = nil
         
         let image2 : UIImage = UIImage(named:"\(dic2).png")!
         diceImage2.removeFromSuperview()
@@ -61,7 +62,7 @@ class ViewController: UIViewController {
         
         diceImage2.frame = CGRect(x: poc3, y: poc4, width: 50, height: 50)
         view.addSubview(diceImage2)
-       // diceImage2.image = nil
+      
       
         print("\(dic1)  \(dic2)")
     }
@@ -78,6 +79,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func unwindToMainMenue(sender: UIStoryboardSegue){
+        let sourceViewController = sender.sourceViewController
+    
+    }
 }
 
